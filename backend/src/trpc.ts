fdbf791c98f0420929b0be778b76f5caa9e42331
Mekhriddin <mirzoevmisha7@gmail.com>
2025-04-27 08:15:13 +0200

    Feat: added route with zod and updated fake data with lodash

--- backend/src/trpc.ts
+++ backend/src/trpc.ts
@@ -1,18 +1,23 @@
 import { initTRPC } from '@trpc/server';
+import _ from 'lodash';
+import { z } from 'zod';
 
-const ideas = [
-  { nick: 'cool-idea-nick-1', name: 'Idea 1', description: 'Description of idea 1...' },
-  { nick: 'cool-idea-nick-2', name: 'Idea 2', description: 'Description of idea 2...' },
-  { nick: 'cool-idea-nick-3', name: 'Idea 3', description: 'Description of idea 3...' },
-  { nick: 'cool-idea-nick-4', name: 'Idea 4', description: 'Description of idea 4...' },
-  { nick: 'cool-idea-nick-5', name: 'Idea 5', description: 'Description of idea 5...' },
-];
+const ideas = _.times(100, i => ({
+  nick: `cool-idea-nick-${i}`,
+  name: `Idea ${i}`,
+  description: `Description of idea ${i}... Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+}));
 
 const trpc = initTRPC.create();
 
 export const trpcRouter = trpc.router({
   getIdeas: trpc.procedure.query(() => {
     return { ideas };
+  }),
+  getIdea: trpc.procedure.input(z.object({ ideaNick: z.string() })).query(({ input }) => {
+    const idea = ideas.find(idea => idea.nick === input.ideaNick);
+    
+    return {idea: idea || null};
   }),
 });
 
--- webapp/src/pages/ViewIdeaPage/index.tsx
+++ webapp/src/pages/ViewIdeaPage/index.tsx
@@ -5,17 +5,24 @@ import { ViewIdeaRouteParams } from '../../lib/routes';
 
 export const ViewIdeaPage = () => {
   const { ideaNick } = useParams() as ViewIdeaRouteParams;
-  const { data } = trpc.getIdeas.useQuery();
-  const idea = data?.ideas.find(idea => idea.nick === ideaNick);
+  const { data, error, isLoading, isFetching, isError } = trpc.getIdea.useQuery({ ideaNick });
 
-  if (!idea) {
+  if (isLoading || isFetching) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  if (!data?.idea) {
     return <div>Idea not found</div>;
   }
 
   return (
     <div>
-      <h1>{idea.name}</h1>
-      <p>{idea.description}</p>
+      <h1>{data.idea.name}</h1>
+      <p>{data.idea.description}</p>
     </div>
   );
 };
