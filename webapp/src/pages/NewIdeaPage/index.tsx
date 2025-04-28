4a63de81a641c91e746afd8bf98a9831926ee024
Mekhriddin <mirzoevmisha7@gmail.com>
2025-04-28 20:43:24 +0200

    Feat(NewIdeaPage): add form and create Input and Textarea components

--- webapp/src/pages/NewIdeaPage/index.tsx
+++ webapp/src/pages/NewIdeaPage/index.tsx
@@ -1,9 +1,32 @@
+import { useState } from 'react';
+
 import { Segment } from '../../components/Segment';
+import { Input } from '../../components/Input';
+import { Textarea } from '../../components/Textarea';
 
 export const NewIdeaPage = () => {
+  const [state, setState] = useState({
+    name: '',
+    nick: '',
+    description: '',
+    text: '',
+  });
   return (
     <div>
-      <Segment title="New Idea" size={1} description="Create a new idea" />
+      <Segment title="New Idea" />
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          console.info('Submitted', state);
+        }}
+      >
+        <Input name="name" label="Name" state={state} setState={setState} />
+        <Input name="nick" label="Nick" state={state} setState={setState} />
+        <Input name="description" label="Description" state={state} setState={setState} />
+        <Textarea name="text" label="Text" state={state} setState={setState} />
+
+        <button type="submit">Create Idea</button>
+      </form>
     </div>
   );
 };
