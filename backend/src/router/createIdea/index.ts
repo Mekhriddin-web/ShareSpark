cb01e1d91a455200e031cc5786e28fd44062472e
Mekhriddin <mirzoevmisha7@gmail.com>
2025-05-07 20:05:32 +0200

    Feat: add idea creation to the database

--- backend/src/router/createIdea/index.ts
+++ backend/src/router/createIdea/index.ts
@@ -1,11 +1,18 @@
 import { trpc } from '../../lib/trpc';
-import { ideas } from '../../lib/ideas';
 import { createIdeaTrpcInput } from './input';
 
-export const createIdeaTrpcRoute = trpc.procedure.input(createIdeaTrpcInput).mutation(({ input }) => {
-  if (ideas.find(idea => idea.nick === input.nick)) {
+export const createIdeaTrpcRoute = trpc.procedure.input(createIdeaTrpcInput).mutation(async ({  input, ctx }) => {
+  
+  const exIdea = await ctx.prisma.idea.findUnique({
+    where: {
+      nick: input.nick,
+    },
+  });
+  if (exIdea) {
     throw new Error('Nick already exists');
   }
-  ideas.unshift(input);
+  await ctx.prisma.idea.create({
+    data: input,
+  });
   return true;
 });
--- backend/src/router/getIdea/index.ts
+++ backend/src/router/getIdea/index.ts
@@ -1,10 +1,12 @@
 import { z } from 'zod';
 
 import { trpc } from '../../lib/trpc';
-import { ideas } from '../../lib/ideas';
 
-export const getIdeaTrpcRoute = trpc.procedure.input(z.object({ ideaNick: z.string() })).query(({ input }) => {
-  const idea = ideas.find(idea => idea.nick === input.ideaNick);
-
-  return { idea: idea || null };
+export const getIdeaTrpcRoute = trpc.procedure.input(z.object({ ideaNick: z.string() })).query(async ({ ctx, input }) => {
+  const idea = await ctx.prisma.idea.findUnique({
+    where: {
+      nick: input.ideaNick,
+    }
+  })
+  return { idea };
 });
